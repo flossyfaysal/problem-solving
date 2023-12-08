27af500058c69ed86886eb32caeb6ea396684a07
flossyfaysal <skfaysalalom@gmail.com>
2023-12-08 23:02:55 +0600

    Number line jumps solved

--- number-line-jumps/index.php
+++ number-line-jumps/index.php
@@ -1,2 +1,14 @@
 <?php
 
+function kangaroo( $x1, $v1, $x2, $v2){
+
+    if( $x1 > $x2 ) return "NO";
+
+    if( ($x2-$x1) % ($v1-$v2) == 0 ){
+        return "YES"
+    }else{
+        return "NO";
+    }
+}
+
+kangaroo( 0, 4, 3, 2);
